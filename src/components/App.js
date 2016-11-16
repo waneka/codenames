7c7e9e5608ac64b5df0e8b3346969eed9232856a
waneka <twwaneka@gmail.com>
2016-11-16 09:50:13 -0800

    Simplifiying App file

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,8 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
 
 const App = (props) => {
   return (
     <div>
-      <IndexLink to="/">Home</IndexLink>
-      {' | '}
-      <Link to="/fuel-savings">Demo App</Link>
-      {' | '}
-      <Link to="/about">About</Link>
-      <br/>
       {props.children}
     </div>
   );
