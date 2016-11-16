6a30f34d895fd46131625c989b4d3a52b77663f6
waneka <twwaneka@gmail.com>
2016-11-16 09:49:14 -0800

    Adding thunk to middleware
    Simplifying routes and reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-import fuelSavings from './fuelSavingsReducer';
 import {routerReducer} from 'react-router-redux';
+import game from './game';
 
 const rootReducer = combineReducers({
-  fuelSavings,
+  game,
   routing: routerReducer
 });
 
