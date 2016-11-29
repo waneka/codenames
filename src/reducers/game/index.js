cf3aca95309506f7e9e954b28a7d2b6ededb0af1
Matthew Knudsen <mknudsen01@gmail.com>
2016-11-29 14:08:51 -0600

    Add clue giver board
    add route for clue giver mode
    actions to switch between list and grid
    grid shows all colored items
    most basic confirmation when clicking to reveal a clue

--- src/reducers/game/index.js
+++ src/reducers/game/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
 import * as actions from '~/actions/game';
+import { GRID } from '~/constants/cluegiverModes';
 
-const defaultState = {};
+const defaultState = {
+  cluegiverMode: GRID
+};
 
 const game = (state = defaultState, action) => {
   switch (action.type) {
     case actions.INITIALIZE_GAME:
-      return {
+      return Object.assign({}, defaultState, {
         words: action.words
-      };
+      });
     case actions.TOGGLE_IS_REVEALED: {
       const newWords = _.cloneDeep(state.words).map(word => {
         if (word.id === action.word.id) {
@@ -18,6 +21,11 @@ const game = (state = defaultState, action) => {
       });
       return Object.assign({}, state, {
         words: newWords
+      });
+    }
+    case actions.UPDATE_CLUEGIVER_MODE: {
+      return Object.assign({}, state, {
+        cluegiverMode: action.mode,
       });
     }
     default:
