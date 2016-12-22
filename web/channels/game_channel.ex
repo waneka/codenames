f6ed8a5448b4b3f824e35ce2b1062aaaad0ed784
Srinivas Rao <savinirs@gmail.com>
2016-12-21 20:03:17 -0800

    backend room support

--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -1,22 +1,26 @@
 defmodule Codenames.GameChannel do
   use Phoenix.Channel
+  intercept ["message"]
 
-  def join("game:public", _message, socket) do
-    IO.inspect('joining')
-    {:ok, socket}
-  end
-
-  def join("game:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  def join("game:" <> room_id, _params, socket) do
+    {:ok, assign(socket, :room_id, room_id)}
   end
 
   def handle_in("message", payload, socket) do
-      broadcast! socket, "message", payload
-      {:noreply, socket}
-    end
+    broadcast! socket, "message", payload
+    {:noreply, socket}
+  end
 
-    def handle_out("message", payload, socket) do
+  def handle_out("message", payload, socket) do
+    if current_room?(socket.assigns[:room_id], socket.topic) do
       push socket, "message", payload
+      {:noreply,socket}
+    else
       {:noreply, socket}
     end
+  end
+
+  defp current_room?(room_id, "game:" <> sub_topic) do
+    room_id == sub_topic
+  end
 end
