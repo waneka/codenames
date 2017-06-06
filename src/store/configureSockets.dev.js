895fe4e4ec0fc209ddfc320d7433b1bda89594cd
Srinivas Rao <savinirs@gmail.com>
2017-06-05 17:48:45 -0700

    :bug: handle urls with dashes

--- src/store/configureSockets.dev.js
+++ src/store/configureSockets.dev.js
@@ -5,7 +5,7 @@ export default function configureSockets(path) {
   const socket = new Socket("ws://localhost:4000/socket", {params: {}});
   socket.connect();
 
-  const room = "game:" + path.match(/\/(\w*)(\/|$)/)[1]
+  const room = "game:" + path.match(/\/([^\/]*)(\/|$)/)[1]
   const channel = socket.channel(room, {});
   return channel;
 }
