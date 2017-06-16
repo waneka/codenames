f57701b139cc15a56330e87d90ffc1d2b0ff82af
waneka <twwaneka@gmail.com>
2017-06-15 21:14:56 -0600

    Add whitespace at end of classname strings in order to separate color classes

--- src/components/WordCard/index.js
+++ src/components/WordCard/index.js
@@ -4,12 +4,13 @@ import colorByCharacter from '~/constants/colorByCharacter';
 
 function WordCard(props) {
   const { word, isClues } = props;
-  let cardClasses = `bo--1 bor--5 flex flex--jc--c align-items--center height--80`;
+  let cardClasses = `bo--1 bor--5 flex flex--jc--c align-items--center height--80 `;
+
   if (isClues) {
-    cardClasses += word.isRevealed ? ' opacity--4-10' : ' cursor--pointer';
-    cardClasses += colorByCharacter[word.character] || ' white';
+    cardClasses += word.isRevealed ? 'opacity--4-10 ' : 'cursor--pointer ';
+    cardClasses += colorByCharacter[word.character] || 'white ';
   } else {
-    cardClasses += word.isRevealed && colorByCharacter[word.character] ? colorByCharacter[word.character] : ' white';
+    cardClasses += word.isRevealed && colorByCharacter[word.character] ? colorByCharacter[word.character] : 'white ';
   }
 
   function toggleIsRevealed(word) {
